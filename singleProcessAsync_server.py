6bbf492962b08adf95cd99d95af5877bc0983fca
Doujohner <ku504507274@126.com>
2019-03-18 16:13:00 +0800

    fix some bugs

--- singleProcessAsync_server.py
+++ singleProcessAsync_server.py
@@ -1,10 +1,10 @@
 # coding: utf-8
-
+# select model
 import json
 import socket
 import struct
 import asyncore
-from io import StringIO
+from io import BytesIO
 
 class RPCHandler(asyncore.dispatcher_with_send):
     def __init__(self, sock, addr):
@@ -13,7 +13,7 @@ class RPCHandler(asyncore.dispatcher_with_send):
         self.handlers={
             "ping":self.ping
         }
-        self.rbuf=StringIO
+        self.rbuf=BytesIO
     
     def handle_connect(self):
         print(self.addr,"comes")
@@ -26,7 +26,7 @@ class RPCHandler(asyncore.dispatcher_with_send):
         while True:
             content = self.recv(1024)
             if content:
-                self.rbuf.write(content)
+                self.rbuf.write(self,content)
             if len(content)<1024:
                 break
         self.handle_rpc()
@@ -34,7 +34,7 @@ class RPCHandler(asyncore.dispatcher_with_send):
     def handle_rpc(self): #handle received message
         while True:
             self.rbuf.seek(0)
-            length_prefix = self.rbuf.read(4)
+            length_prefix = self.rbuf.read(self,4)
             if len(length_prefix)<4:
                 break
             length,=struct.unpack("I",length_prefix)
@@ -42,11 +42,26 @@ class RPCHandler(asyncore.dispatcher_with_send):
             if len(body)<length:
                 break
             request=json.loads(body)
-            #todo
+            in_=request["in"]
+            params = request["params"]
+            print(in_,params)
+            handler = self.handlers[in_]
+            handler(params)
+            left=self.rbuf.getvalue()[length+4]#the whole buffer
+            self.rbuf=BytesIO()#initialize buffer
+            # self.rbuf=
+            self.rbuf.write(left)
+        self.rbuf.seek(0,2)#change stream to the end
+        
     def ping(self,params):
-        pass
+        self.send_result("pong",params)
     def send_result(self,out,result): #write buffer
-        pass
+        response={"out":out,"result":result}
+        body = json.dumps(response)
+        length_prefix=struct.pack("I",len(body))
+        self.send(length_prefix)
+        self.send(body.encode())
+
 class RPCServer(asyncore.dispatcher):
     def __init__(self, host, port):
         asyncore.dispatcher.__init__(self)
@@ -54,10 +69,14 @@ class RPCServer(asyncore.dispatcher):
         self.set_reuse_addr()
         self.bind((host,port))
         self.listen(1)
+
     def handle_accept(self):
         pair=self.accept()
         if pair is not None:
             sock,addr=pair
             RPCHandler(sock,addr)
 
-    
+
+if __name__=='__main__':
+    RPCServer("localhost",8100)
+    asyncore.loop()
